7ceea1d1c4cd10ce91747e84390318186dd17d9c
Rails Udemy Course <rails@gmail.com>
2019-07-18 13:58:07 +0700

    modify article controller, new, and application

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,4 @@
-class ArticlesController < ApplicationController
+ class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   # GET /articles
